c5d8d6aa0f6247f86692d110339ca734c91c3a0f
NaxoAzul <ignacio.sepulveda48@inacapmail.cl>
2026-05-03 21:03:07 -0400

    avance login y register

--- js/login.js
+++ js/login.js
@@ -1,33 +1,49 @@
-const users = [
-    { user: "user1@sportclub.cl", password: "1234", role: "usuario" },
-    { user: "coach1@sportclub.cl", password: "1234", role: "coach" },
-    { user: "admin1@sportclub.cl", password: "1234", role: "admin" }
-];
-
 const form = document.querySelector("form");
 const mensaje = document.querySelector(".mensaje");
 
-form.addEventListener("submit", function(e) {
+form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    const usuario = users.find(u => u.user === email && u.password === password);
+    try {
+        const response = await fetch("http://localhost:3000/api/auth/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        });
 
-    if (usuario) {
-        localStorage.setItem("user", JSON.stringify(usuario));
+        const data = await response.json();
 
-        if (usuario.role === "usuario") {
-            window.location.href = "dashboard-user.html";
-        } else if (usuario.role === "coach") {
-            window.location.href = "dashboard-coach.html";
-        } else if (usuario.role === "admin") {
-            window.location.href = "dashboard-admin.html";
+        if (data.ok) {
+            // guardar datos
+            localStorage.setItem("token", data.data.token);
+            localStorage.setItem("user", JSON.stringify(data.data.user));
+
+            const role = data.data.user.role;
+
+            // redirección
+            if (role === "user") {
+                window.location.href = "dashboard-user.html";
+            } else if (role === "coach") {
+                window.location.href = "dashboard-coach.html";
+            } else if (role === "admin") {
+                window.location.href = "dashboard-admin.html";
+            }
+
+        } else {
+            mensaje.textContent = data.message;
+            mensaje.style.color = "red";
         }
 
-    } else {
-        mensaje.textContent = "Correo o contraseña incorrectos";
+    } catch (error) {
+        mensaje.textContent = "Error al conectar con el servidor";
         mensaje.style.color = "red";
     }
 });
